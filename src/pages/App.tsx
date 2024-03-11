e00ca88f3878e3b7cdc890651bd6769e275ff875
drapkinamaria <drapkinamaria02@gmail.com>
2024-03-11 17:48:15 +0500

    module3-task1

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,4 +1,11 @@
-import Main from './Main';
+import Main from './main';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import {AppRoute, AuthorizationStatus} from '../const';
+import Login from './login';
+import Offer from './offer';
+import NotFound from './not-found';
+import PrivateRoute from './private-route';
+import Favorites from './favorites';
 
 type AppProps = {
   rentalOffersCount: number;
@@ -6,9 +13,36 @@ type AppProps = {
 
 function App({ rentalOffersCount }: AppProps):JSX.Element {
   return (
-    <div>
-      <Main rentalOffersCount={rentalOffersCount} />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Root}
+          element={<Main rentalOffersCount={rentalOffersCount} />}
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<Login/>}
+        />
+        <Route
+          path={AppRoute.Favorites}
+          element={
+            <PrivateRoute
+              authorizationStatus={AuthorizationStatus.NoAuth}
+            >
+              <Favorites/>
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path={AppRoute.Offer}
+          element={<Offer/>}
+        />
+        <Route
+          path='*'
+          element={<NotFound/>}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/pages/not-found.tsx
+++ src/pages/not-found.tsx
@@ -1,7 +1,10 @@
+import {Link} from 'react-router-dom';
+
 function NotFound(): JSX.Element {
   return (
     <div style={{paddingLeft: 20}}>
       <h1>404 Not Found</h1>
+      <Link to='/'>Перейти на главную страницу</Link>
     </div>
   );
 }
