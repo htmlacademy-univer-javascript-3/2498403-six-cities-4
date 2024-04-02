272491db007694b69def39d2aeb024f70edc1faa
drapkinamaria <drapkinamaria02@gmail.com>
2024-04-02 15:38:23 +0500

    module4-task1 fixes 2

--- src/pages/MainEmpty.tsx
+++ src/pages/MainEmpty.tsx
@@ -1,13 +1,48 @@
 import {Header} from '../components/header/Header';
-import {CitiesList} from '../components/citiesList/CitiesList';
 
-function MainEmpty(cities): JSX.Element {
+function MainEmpty(): JSX.Element {
   return (
     <div className="page page--gray page--main">
       <Header />
 
       <main className="page__main page__main--index page__main--index-empty">
-        <CitiesList cities={cities}></CitiesList>
+        <h1 className="visually-hidden">Cities</h1>
+        <div className="tabs">
+          <section className="locations container">
+            <ul className="locations__list tabs__list">
+              <li className="locations__item">
+                <a className="locations__item-link tabs__item" href="#">
+                  <span>Paris</span>
+                </a>
+              </li>
+              <li className="locations__item">
+                <a className="locations__item-link tabs__item" href="#">
+                  <span>Cologne</span>
+                </a>
+              </li>
+              <li className="locations__item">
+                <a className="locations__item-link tabs__item" href="#">
+                  <span>Brussels</span>
+                </a>
+              </li>
+              <li className="locations__item">
+                <a className="locations__item-link tabs__item">
+                  <span>Amsterdam</span>
+                </a>
+              </li>
+              <li className="locations__item">
+                <a className="locations__item-link tabs__item" href="#">
+                  <span>Hamburg</span>
+                </a>
+              </li>
+              <li className="locations__item">
+                <a className="locations__item-link tabs__item tabs__item--active" href="#">
+                  <span>Dusseldorf</span>
+                </a>
+              </li>
+            </ul>
+          </section>
+        </div>
         <div className="cities">
           <div className="cities__places-container cities__places-container--empty container">
             <section className="cities__no-places">
